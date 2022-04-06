ec861f930eb0549308adbd9b768767f16d69e7c4
Robin de Mourat <robin.demourat@gmail.com>
2022-04-06 07:18:30 +0200

    tenshinhan viz : fix little issue

--- communautes_politiques/tenshin-han-viz/example.js
+++ communautes_politiques/tenshin-han-viz/example.js
@@ -8,11 +8,11 @@ const graph2017 = gexf.parse(Graph, fs.readFileSync('melenchon_2017.gexf', 'utf8
 const graph2022 = gexf.parse(Graph, fs.readFileSync('melenchon_2022.gexf', 'utf8'));
 
 const mergedGraph = mergeGraphs(graph2017, graph2022);
-const tripartiteGraph = layout(mergedGraph);
-
 // write graph
 const gexfString1 = gexf.write(mergedGraph);
 fs.writeFileSync('mergedGraph.gexf', gexfString1, 'utf8')
+
+const tripartiteGraph = layout(mergedGraph);
 
 // write graph
 const gexfString2 = gexf.write(tripartiteGraph);

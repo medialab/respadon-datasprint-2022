8027c4593dcb582f89beff9b8c84d27e35321465
Paul Girard <paul@ouestware.com>
2022-04-12 11:33:56 +0200

    [genome] solr them queries debugged - POST /query is not available switching to GET /select - using url_search field for url filter

--- genome/hyphe_corpus_content_solr_queries.py
+++ genome/hyphe_corpus_content_solr_queries.py
@@ -2,11 +2,14 @@ from collections import defaultdict
 import os
 import json
 import csv
+from urllib.parse import urlencode
+from ural import normalize_url, force_protocol
+
 
 CORPUS_FILE = "génome élections 2002 v2 complet.json"
 THEMES_TERMS = ["ELECTIONS", "terms-tfudf_election2002_harmonized-data.csv"]
 DATA_PATH = "bcweb_data"
-SOLR_URL = "http://nemo10.bnf.fr:8983/solr/netarchivebuilder/query"
+SOLR_URL = "http://nemo10.bnf.fr:8983/solr/netarchivebuilder/select"
 
 
 themes_list = set(
@@ -37,33 +40,44 @@ with open(os.path.join(DATA_PATH, *THEMES_TERMS), "r") as f:
         if term["new_term"] in themes_list:
             terms[term["new_term"]].add(term["term"])
 
-terms_facets = {
-    theme: {"type": "query", "q": f'text:{" OR ".join(ts)}'}
-    for (theme, ts) in terms.items()
-}
+terms_facets = {"facet.query": [" OR ".join(ts) for (theme, ts) in terms.items()]}
 
 curl_bash_script = []
-all_bodies = []
+all_queries = []
 
 with open(os.path.join(DATA_PATH, CORPUS_FILE), "r") as webs_f:
     hyphe_incunable = json.load(webs_f)
     for web_entity in hyphe_incunable["webentities"]:
         # using filter looks like an optimization but not sure...
-        prefixes_filters = [
-            f"filter(url:{url}*)" for url in web_entity["PREFIXES AS URL"]
-        ]
-        search_query_body = {
-            "query": " OR ".join(prefixes_filters),
-            "facet": terms_facets,
-        }
-        curl_bash_script.append(
-            f"curl {SOLR_URL} -d '{json.dumps(search_query_body)}'' > {web_entity['ID']}.json"
+        prefix_normalize = set(
+            [
+                # mimic wayback url nomralization https://github.com/netarchivesuite/solrwayback/blob/master/src/bundle/solrconfig_7.7.3/solr/configsets/netarchivebuilder/conf/schema.xml#L157
+                force_protocol(
+                    normalize_url(
+                        url, strip_irrelevant_subdomains=True, strip_trailing_slash=True
+                    ),
+                    protocol="http",
+                )
+                for url in web_entity["PREFIXES AS URL"]
+            ]
         )
-        all_bodies.append(search_query_body)
+        prefixes_filters = [f'url_search:"{url}"*' for url in prefix_normalize]
+        if len(prefixes_filters) > 0:
 
+            query_string = urlencode(
+                {"q": " OR ".join(prefixes_filters)} | terms_facets
+            )
+
+            query = f"{SOLR_URL}?{query_string}"
+            all_queries.append(query)
+            curl_bash_script.append(f"curl '{query}' > {web_entity['ID']}.json")
+        else:
+            print(
+                f'missing http without www prefix in {web_entity["NAME"]} {web_entity["PREFIXES AS URL"]}'
+            )
 with open(
     os.path.join(DATA_PATH, "solr_webentities_themes_search_bodies.json"), "w"
 ) as f:
-    json.dump(all_bodies, f, indent=2)
+    json.dump(all_queries, f, indent=2)
 with open(os.path.join(DATA_PATH, "solr_webentities_themes_curl.sh"), "w") as f:
     f.write("\n".join(curl_bash_script))
